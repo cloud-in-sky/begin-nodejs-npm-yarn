fd2ea3d7ca98001ced07a09c905c1666db6db777
admin1 <admin1@mteden.local>
2018-07-25 08:49:35 +1200

    add emu bats

---
+++ start-emu.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\Users\change-to-your-user-name-here\AppData\Local\Android\Sdk\emulator\emulator.exe -avd "Nexus_5_API_23-or-your-avd-name" -wipe-data -no-snapshot
---
+++ stop-emu.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\Users\Hi\AppData\Local\Android\Sdk\platform-tools\adb emu kill
